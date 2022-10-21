3be0e79a728f603be09577dedaac10a4add529a0
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-10-21 05:23:50 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,44 @@
+/*
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: Micheal Zamansky
+Assignment: Lab 6 B
+I wrote a program that functions implementing Caesar cipher encryption.
+*/
+#include <iostream>
+#include "funcs.h"
 #include <iostream>
 
+using namespace std;
+char shiftChar(char c, int rshift);
 
-#include "funcs.h"
-
+string encryptCaesar(string plaintext, int rshift);
 int main()
 {
-  return 0;
+string s;
+int n;
+cout<<"Enter plaintext:"<<endl;
+getline(cin, s);
+cout<<"Enter shift:"<<endl;
+cin >> n;
+
+cout<<"Ciphertext: "<<encryptCaesar(s, n)<<endl;
+return 0;
+}
+char shiftChar(char c, int rshift) {
+if(c>='a' && c <='z')
+return 'a' + ((c - 'a' + rshift) % 26);
+else
+return 'A' + ((c - 'A' + rshift) % 26);
+}
+
+string encryptCaesar(string plaintext, int rshift) {
+for(int i=0;i<plaintext.length();i++) {
+char c = plaintext[i];
+if((c>='a' && c <='z') || (c>='A' && c <='Z')) {
+plaintext[i] = shiftChar( plaintext[i], rshift);
+}
+}
+return plaintext;
+return 0;
 }
